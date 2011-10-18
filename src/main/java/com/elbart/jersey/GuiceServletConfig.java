06a866d754d84ae8563f3ba9320191f424086874
Tim Eggert <tim@elbart.com>
2011-10-19 01:11:22 +0200

    added moved files

---
+++ src/main/java/com/elbart/jersey/GuiceServletConfig.java
@@ -0,0 +1,13 @@
+package com.elbart.jersey;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.servlet.GuiceServletContextListener;
+
+public class GuiceServletConfig extends GuiceServletContextListener {
+
+    @Override
+    protected Injector getInjector() {
+        return Guice.createInjector(new MyGuiceConfig());
+    }
+}
---
+++ src/main/java/com/elbart/jersey/Main.java
@@ -0,0 +1,9 @@
+package com.elbart.jersey;
+
+public class Main {
+
+    public static void main(String[] argv){
+        System.out.println("Hello World");
+    }
+
+}
