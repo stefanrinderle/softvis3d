22a71e59b6376684575b0ece0887f921b5d95ba9
Stefan Rinderle <stefan@rinderle.info>
2014-09-22 13:41:56 +0200

    Click interaction is working.

--- src/main/resources/static/js/ThreeScene.js
+++ src/main/resources/static/js/ThreeScene.js
@@ -1,188 +1,148 @@
+var container, stats;
+var camera, scene, projector, renderer;
+
 var controls;
+var objects = [];
 
-var ThreeScene = function(container) {
-	
-	this.container = container;
-	
-	this.scene = new THREE.Scene(); 
-	
-	this.camera = new THREE.PerspectiveCamera(60, window.innerWidth
-			/ window.innerHeight, 1, 10000);
-	
-	this.renderer = new THREE.WebGLRenderer({
-		antialias : true
-	});
-	
-	var stats = new Stats();
-	
-	this.init = function() {
-		this.startRenderer();
-		this.setLight();
-		
-		this.createCenterCube();
-		
-		window.addEventListener('resize', this.onWindowResize, false);
-		this.onWindowResize();
-		
-		animate();
-		
-		this.render();
-		
-		this.camera.position.set(0, 4000, 4000);
-	},
-	
-	this.startRenderer = function () {
-		this.camera.position.set(0, 0, 0);
+var containerWidth, containerHeight;
 
-		this.renderer.setClearColor(0xffffff, 1);
-		
-		controls = new THREE.OrbitControls(this.camera, this.container);
-		
-		var cameraRef = this.camera;
-		var sceneRef = this.scene;
-		var rendererRef = this.renderer;
-		controls.addEventListener('change', function render() {
-			rendererRef.render(sceneRef, cameraRef);
-			//stats.update();
-		});
-		
-		this.container.appendChild(this.renderer.domElement);
-		
-		this.showStats();
-	},
-	
-	this.setLight = function () {
-		var light = new THREE.DirectionalLight( 0xaaaaaa);
-	    light.position.set( 1, 0, 0 ).normalize();
-	    this.scene.add( light );
+function init(container, showStats) {
+    var info = document.createElement( 'div' );
+    info.style.position = 'absolute';
+    info.style.bottom = '10px';
+    info.style.width = '100%';
+    info.style.textAlign = 'center';
+    container.appendChild( info );
 
-	    var light = new THREE.DirectionalLight( 0xcccccc);
-	    light.position.set( -1, 0, 0 ).normalize();
-	    this.scene.add( light );
-	    
-	    var light = new THREE.DirectionalLight( 0xddddddd);
-	    light.position.set( 0, 0, 1 ).normalize();
-	    this.scene.add( light );
-	    
-	    var light = new THREE.DirectionalLight( 0xeeeeee);
-	    light.position.set( 0, 0, -1 ).normalize();
-	    this.scene.add( light );
-	    
-	    var directionalLight = new THREE.DirectionalLight( 0xffffff);
-	    directionalLight.position.set( 0, 1, 0 );
-	    this.scene.add( directionalLight );
-	},
-	
-	this.showStats = function() {
-		// show stats
-		//stats.domElement.style.zIndex = 100;
-		//this.container.appendChild(stats.domElement);
+    // header of sonar is 70 px + metric select form 30 px + footer 50 px
+    // sidebar 200px
+    containerHeight = window.innerHeight - 150;
+    containerWidth = window.innerWidth - 200;
 
-		stats.setMode(0);
+    camera = new THREE.PerspectiveCamera( 45, containerWidth / containerHeight, 1, 10000 );
+    camera.position.set( 0, 300, 500 );
 
-        // Align top-left
+    scene = new THREE.Scene();
+
+    projector = new THREE.Projector();
+
+    renderer = new THREE.WebGLRenderer({
+        antialias: true,
+        alpha: true
+    });
+
+    container.appendChild( renderer.domElement );
+
+    controls = new THREE.OrbitControls(camera, this.container);
+
+    if (showStats) {
+        stats = new Stats();
         stats.domElement.style.position = 'absolute';
-        stats.domElement.style.left = '0px';
         stats.domElement.style.bottom = '0px';
+        container.appendChild( stats.domElement );
 
-        document.body.appendChild( stats.domElement );
+        controls.addEventListener('change', function render() {
+            stats.update();
+        });
+    }
 
-        setInterval( function () {
+    document.addEventListener( 'mousedown', onDocumentMouseDown, false );
 
-            stats.begin();
+    window.addEventListener( 'resize', onWindowResize, false );
 
-            // your code goes here
+    onWindowResize();
+}
 
-            stats.end();
+function onWindowResize() {
+    containerWidth = window.innerWidth - 200;
+    containerHeight = window.innerHeight - 150;
 
-        }, 1000 / 60 );
-	},
-	
-	this.createCenterCube = function() {
-		// create center cube for better navigation
-		var centerCube = new THREE.BoxGeometry(10, 10, 10);
-		var greenMaterial = new THREE.MeshBasicMaterial({
-			color : 0x00ff00
-		});
-		position = new Array();
-		position.x = 0;
-		position.y = 0;
-		position.z = 0;
+    renderer.setSize( containerWidth, containerHeight );
+    camera.aspect = containerWidth / containerHeight;
+    camera.updateProjectionMatrix();
+}
 
-		var mesh = new THREE.Mesh(centerCube, greenMaterial);
-		mesh.position.x = position.x;
-		mesh.position.y = position.y;
-		mesh.position.z = position.z;
-		mesh.updateMatrix();
-		mesh.matrixAutoUpdate = false;
+function onDocumentMouseDown( event ) {
 
-		this.scene.add(mesh);
-	},
+    event.preventDefault();
 
-	this.onWindowResize = function() {
-		this.camera.aspect = window.innerWidth / window.innerHeight;
-		this.camera.updateProjectionMatrix();
+    // header of sonar is 70 px + metric select form 30 px
+    var mouseVector = new THREE.Vector3(
+            2 * ((event.clientX - 170) / containerWidth) - 1,
+            1 - 2 * ((event.clientY - 100) / containerHeight),
+        0.5 );
 
-		// header of sonar is 70 px + footer 50 px + metric select form 30 px 
-		// sidebar 200px
-		this.renderer.setSize(window.innerWidth - 200, window.innerHeight - 150);
+    var raycaster = projector.pickingRay( mouseVector.clone(), camera );
 
-		this.render();
-	},
+    var intersects = raycaster.intersectObjects( objects );
 
-	this.render = function() {
-		this.renderer.render(this.scene, this.camera);
-		stats.update();
-	},
-	
-	this.createBox = function(geometry1, material1, position1) {
-		var mesh1 = new THREE.Mesh(geometry1, material1);
-		mesh1.position.x = position1.x;
-		mesh1.position.y = position1.y;
-		mesh1.position.z = position1.z;
-		mesh1.updateMatrix();
-		mesh1.matrixAutoUpdate = false;
+    if ( intersects.length > 0 ) {
 
-		this.scene.add(mesh1);
-	},
+        intersects[ 0 ].object.material.color.setHex( Math.random() * 0xffffff );
 
-	this.drawText = function(text, position2, size) {
-        // ///// draw text on canvas /////////
+        console.log(intersects[ 0 ].object.softviz3dId);
 
-        // create a canvas element
-        var canvas1 = document.createElement('canvas');
-        var context1 = canvas1.getContext('2d');
-        
-        context1.font = "Bold " + size + "px Arial";
-        // context1.fillStyle = "rgba(255,0,0,0.95)";
-        context1.fillText(text, 0, size);
-
-        var value = context1.measureText(text).width;
-        canvas1.width = value;
-        canvas1.height = size;
-        
-        context1.font = size + "px Arial";
-        context1.textBaseline = "bottom";
-        context1.fillStyle = "rgba(0,0,0,0.95)";
-        context1.fillText(text, 0, size);
-        
-        // canvas contents will be used for a texture
-        var texture1 = new THREE.Texture(canvas1);
-        texture1.needsUpdate = true;
-          
-        var material1 = new THREE.MeshBasicMaterial( {map: texture1, side:THREE.DoubleSide } );
-        material1.transparent = true;
-
-        var mesh1 = new THREE.Mesh(
-            new THREE.PlaneGeometry(canvas1.width, canvas1.height),
-            material1
-          );
-        mesh1.position = position2;
-        this.scene.add( mesh1 );
-    };
-};
+//        var particle = new THREE.Sprite( particleMaterial );
+//        particle.position.copy( intersects[ 0 ].point );
+//        particle.scale.x = particle.scale.y = 16;
+//        scene.add( particle );
+    }
+}
 
 function animate() {
-	requestAnimationFrame(animate);
-	controls.update();
+    requestAnimationFrame(animate);
+
+    render();
 }
+
+function render() {
+    renderer.render( scene, camera );
+}
+
+function createCenterCube() {
+    var cubeMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00, opacity: 0.5 } );
+    var geometry = new THREE.BoxGeometry( 100, 100, 100 );
+
+    var position = new Array();
+    position.x = 0;
+    position.y = 0;
+    position.z = 0;
+
+    createBox(geometry, cubeMaterial, position, 0);
+};
+
+function createBox(geometry, material, position, id) {
+    var object = new THREE.Mesh( geometry,  material);
+
+    object.position.x = position.x;
+    object.position.y = position.y;
+    object.position.z = position.z;
+
+    object.softviz3dId = id;
+
+    objects.push( object );
+
+    scene.add(object)
+};
+
+function setLight() {
+    var light = new THREE.DirectionalLight(0xaaaaaa);
+    light.position.set(1, 0, 0).normalize();
+    scene.add(light);
+
+    var light = new THREE.DirectionalLight(0xcccccc);
+    light.position.set(-1, 0, 0).normalize();
+    scene.add(light);
+
+    var light = new THREE.DirectionalLight(0xddddddd);
+    light.position.set(0, 0, 1).normalize();
+    scene.add(light);
+
+    var light = new THREE.DirectionalLight(0xeeeeee);
+    light.position.set(0, 0, -1).normalize();
+    scene.add(light);
+
+    var directionalLight = new THREE.DirectionalLight(0xffffff);
+    directionalLight.position.set(0, 1, 0);
+    scene.add(directionalLight);
+};
