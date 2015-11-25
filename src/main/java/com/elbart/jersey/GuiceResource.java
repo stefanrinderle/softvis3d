5a63e2f8b01d3859bd4d6ce969456fbc231b9434
Priyabrata Dash <bobquest33@gmail.com>
2015-11-25 16:28:14 +0530

    updated user resource

--- src/main/java/com/elbart/jersey/GuiceResource.java
+++ src/main/java/com/elbart/jersey/GuiceResource.java
@@ -3,20 +3,94 @@ package com.elbart.jersey;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.OPTIONS;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.QueryParam;
-
 import com.google.inject.servlet.RequestScoped;
+import java.util.*;
+import java.io.IOException;
 
-@Path("/job")
+
+@Path("/")
 @RequestScoped
 public class GuiceResource {
 
-    @QueryParam("x")
-    String x;
+	
+	   UserDAO userDao = new UserDAO();
+	   private static final String SUCCESS_RESULT="<result>success</result>";
+	   private static final String FAILURE_RESULT="<result>failure</result>";
 
-    @GET
-    @Produces("text/plain")
-    public String getIt() {
-        return "Hello From Guice: " + x;
-    }
+
+	   @GET
+	   @Path("/users")
+	   @Produces(MediaType.APPLICATION_XML)
+	   public List<User> getUsers(){
+	      return userDao.getAllUsers();
+	   }
+
+	   @GET
+	   @Path("/users/{userid}")
+	   @Produces(MediaType.APPLICATION_XML)
+	   public User getUser(@PathParam("userid") int userid){
+	      return userDao.getUser(userid);
+	   }
+
+	   @PUT
+	   @Path("/users")
+	   @Produces(MediaType.APPLICATION_XML)
+	   @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	   public String createUser(@FormParam("id") int id,
+	      @FormParam("name") String name,
+	      @FormParam("profession") String profession,
+	      @Context HttpServletResponse servletResponse) throws IOException{
+	      User user = new User(id, name, profession);
+	      int result = userDao.addUser(user);
+	      if(result == 1){
+	         return SUCCESS_RESULT;
+	      }
+	      return FAILURE_RESULT;
+	   }
+
+	   @POST
+	   @Path("/users")
+	   @Produces(MediaType.APPLICATION_XML)
+	   @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	   public String updateUser(@FormParam("id") int id,
+	      @FormParam("name") String name,
+	      @FormParam("profession") String profession,
+	      @Context HttpServletResponse servletResponse) throws IOException{
+	      User user = new User(id, name, profession);
+	      int result = userDao.updateUser(user);
+	      if(result == 1){
+	         return SUCCESS_RESULT;
+	      }
+	      return FAILURE_RESULT;
+	   }
+
+	   @DELETE
+	   @Path("/users/{userid}")
+	   @Produces(MediaType.APPLICATION_XML)
+	   public String deleteUser(@PathParam("userid") int userid){
+	      int result = userDao.deleteUser(userid);
+	      if(result == 1){
+	         return SUCCESS_RESULT;
+	      }
+	      return FAILURE_RESULT;
+	   }
+
+	   @OPTIONS
+	   @Path("/users")
+	   @Produces(MediaType.APPLICATION_XML)
+	   public String getSupportedOperations(){
+	      return "<operations>GET, PUT, POST, DELETE</operations>";
+	   }
+	   
 }
---
+++ src/main/java/com/elbart/jersey/User.java
@@ -0,0 +1,62 @@
+package com.elbart.jersey;
+
+import java.io.Serializable;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "user")
+public class User {
+	  private static final long serialVersionUID = 1L;
+	   private int id;
+	   private String name;
+	   private String profession;
+
+	   public User(){}
+
+	   public User(int id, String name, String profession){
+	      this.id = id;
+	      this.name = name;
+	      this.profession = profession;
+	   }
+
+	   public int getId() {
+	      return id;
+	   }
+	   @XmlElement
+	   public void setId(int id) {
+	      this.id = id;
+	   }
+	   public String getName() {
+	      return name;
+	   }
+	   @XmlElement
+	      public void setName(String name) {
+	      this.name = name;
+	   }
+	   public String getProfession() {
+	      return profession;
+	   }
+	   @XmlElement
+	   public void setProfession(String profession) {
+	      this.profession = profession;
+	   }	
+
+	   @Override
+	   public boolean equals(Object object){
+	      if(object == null){
+	         return false;
+	      }else if(!(object instanceof User)){
+	         return false;
+	      }else {
+	         User user = (User)object;
+	         if(id == user.getId()
+	            && name.equals(user.getName())
+	            && profession.equals(user.getProfession())
+	         ){
+	            return true;
+	         }			
+	      }
+	      return false;
+	   }	
+}
