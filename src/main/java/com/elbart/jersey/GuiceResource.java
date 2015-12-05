cc3903be4eceaf33a30b3ace40f40b37e2675361
Stefan Rinderle <stefan@rinderle.info>
2015-12-05 19:35:43 +0100

    Cleanup 1

--- src/main/java/com/elbart/jersey/GuiceResource.java
+++ src/main/java/com/elbart/jersey/GuiceResource.java
@@ -1,115 +1,75 @@
 package com.elbart.jersey;
 
+import com.google.gson.Gson;
 import com.google.inject.Inject;
 import com.google.inject.servlet.RequestScoped;
-import de.rinderle.softvis3d.VisualizationWebserviceHandler;
-import java.io.IOException;
+import de.rinderle.softvis3d.VisualizationProcessor;
+import de.rinderle.softvis3d.VisualizationSettings;
+import de.rinderle.softvis3d.domain.LayoutViewType;
+import de.rinderle.softvis3d.domain.SnapshotStorageKey;
+import de.rinderle.softvis3d.domain.SnapshotTreeResult;
+import de.rinderle.softvis3d.domain.VisualizationRequest;
+import de.rinderle.softvis3d.domain.graph.ResultPlatform;
+import de.rinderle.softvis3d.domain.tree.RootTreeNode;
+import de.rinderle.softvis3d.domain.tree.TreeNode;
+import de.rinderle.softvis3d.domain.tree.TreeNodeType;
+import de.rinderle.softvis3d.domain.tree.ValueTreeNode;
+import java.util.ArrayList;
 import java.util.List;
-import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.FormParam;
+import java.util.Map;
 import javax.ws.rs.GET;
 import javax.ws.rs.OPTIONS;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import org.eclipse.jetty.util.Jetty;
 
 @Path("/")
 @RequestScoped
 public class GuiceResource {
 
   @Inject
-  UserDAO userDao;
-
-  @Inject
-  VisualizationWebserviceHandler webserviceHandler;
-
-  @Inject
-  JettyPreProcessingService jettyPreProcessingService;
+  private VisualizationProcessor visualizationProcessor;
 
   private static final String SUCCESS_RESULT = "<result>success</result>";
   private static final String FAILURE_RESULT = "<result>failure</result>";
 
   @GET
   @Path("/users")
-  @Produces(MediaType.APPLICATION_XML)
-  public List<User> getUsers() {
-
-
-    List<User> users = userDao.getAllUsers();
+  @Produces(MediaType.APPLICATION_JSON)
+  public String getUsers() {
 
     try {
-      String result = webserviceHandler.handleRequest(jettyPreProcessingService);
-      users.get(0).setName(result);
+      VisualizationSettings settings = new VisualizationSettings();
+      VisualizationRequest requestDTO =
+              new VisualizationRequest(1, LayoutViewType.CITY, 1, 20);
+      SnapshotTreeResult snapshotTreeResult = createSnapShotTreeResult(requestDTO);
+
+      Map<Integer, ResultPlatform> result = visualizationProcessor.visualize(1, settings, requestDTO, snapshotTreeResult);
+
+      return new Gson().toJson(result);
     } catch (Exception e) {
       e.printStackTrace();
+      return null;
     }
-
-    return users;
   }
 
-  @GET
-  @Path("/users/{userid}")
-  @Produces(MediaType.APPLICATION_XML)
-  public User getUser(@PathParam("userid") int userid) {
-    return userDao.getUser(userid);
-  }
+  private SnapshotTreeResult createSnapShotTreeResult(VisualizationRequest requestDTO ) {
+      final RootTreeNode result = new RootTreeNode(requestDTO.getRootSnapshotId());
 
-  @PUT
-  @Path("/users")
-  @Produces(MediaType.APPLICATION_XML)
-  @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-  public String createUser(@FormParam("id") int id,
-    @FormParam("name") String name,
-    @FormParam("profession") String profession,
-    @Context HttpServletResponse servletResponse) throws IOException {
+      TreeNode node2 = new ValueTreeNode(2, result, 1, TreeNodeType.TREE, "2", 1.3, 1.5, 2);
+      TreeNode node3 = new ValueTreeNode(3, result, 1, TreeNodeType.TREE, "3", 1.7, 1.8, 1);
+      result.addChildrenNode("2", node2);
+      result.addChildrenNode("3", node3);
 
-    User user = new User(id, name, profession);
-    int result = userDao.addUser(user);
-    if (result == 1) {
-      return SUCCESS_RESULT;
-    }
-    return FAILURE_RESULT;
-  }
-
-  @POST
-  @Path("/users")
-  @Produces(MediaType.APPLICATION_XML)
-  @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-  public String updateUser(@FormParam("id") int id,
-    @FormParam("name") String name,
-    @FormParam("profession") String profession,
-    @Context HttpServletResponse servletResponse) throws IOException {
-    User user = new User(id, name, profession);
-    int result = userDao.updateUser(user);
-    if (result == 1) {
-      return SUCCESS_RESULT;
-    }
-    return FAILURE_RESULT;
-  }
-
-  @DELETE
-  @Path("/users/{userid}")
-  @Produces(MediaType.APPLICATION_XML)
-  public String deleteUser(@PathParam("userid") int userid) {
-    int result = userDao.deleteUser(userid);
-    if (result == 1) {
-      return SUCCESS_RESULT;
-    }
-    return FAILURE_RESULT;
+    SnapshotStorageKey key = new SnapshotStorageKey(requestDTO);
+    return new SnapshotTreeResult(key, result);
   }
 
   @OPTIONS
   @Path("/users")
   @Produces(MediaType.APPLICATION_XML)
   public String getSupportedOperations() {
-    return "<operations>GET, PUT, POST, DELETE</operations>";
+    return "<operations>GET, POST</operations>";
   }
 
 }
--- src/main/java/com/elbart/jersey/Main.java
+++
@@ -1,9 +0,0 @@
-package com.elbart.jersey;
-
-public class Main {
-
-    public static void main(String[] argv){
-        System.out.println("Hello World");
-    }
-
-}
