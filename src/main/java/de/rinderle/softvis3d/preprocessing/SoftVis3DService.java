c65c6513aa5143c8d17e69121281903780d7e4b7
Stefan Rinderle <stefan@rinderle.info>
2015-12-11 14:40:52 -0600

    Adjust webservice example.

--- src/main/java/de/rinderle/softvis3d/preprocessing/SoftVis3DService.java
+++ src/main/java/de/rinderle/softvis3d/preprocessing/SoftVis3DService.java
@@ -1,12 +1,12 @@
 package de.rinderle.softvis3d.preprocessing;
 
 import com.google.inject.Inject;
+import de.rinderle.softvis3d.VisualizationAdditionalInfos;
 import de.rinderle.softvis3d.VisualizationProcessor;
 import de.rinderle.softvis3d.VisualizationSettings;
 import de.rinderle.softvis3d.domain.LayoutViewType;
-import de.rinderle.softvis3d.domain.SnapshotStorageKey;
+import de.rinderle.softvis3d.domain.MinMaxValue;
 import de.rinderle.softvis3d.domain.SnapshotTreeResult;
-import de.rinderle.softvis3d.domain.VisualizationRequest;
 import de.rinderle.softvis3d.domain.graph.ResultPlatform;
 import de.rinderle.softvis3d.domain.tree.RootTreeNode;
 import de.rinderle.softvis3d.domain.tree.TreeNode;
@@ -26,30 +26,35 @@ public class SoftVis3DService {
     public Map<Integer, ResultPlatform> getExampleResult() throws Exception {
 
         final VisualizationSettings settings = new VisualizationSettings();
-        final VisualizationRequest requestDTO = createExampleVisualizationRequest();
-        final SnapshotTreeResult snapshotTreeResult = createSnapShotTreeResult(requestDTO);
-
+        final SnapshotTreeResult snapshotTreeResult = createSnapShotTreeResult(1);
+        final VisualizationAdditionalInfos additionalInfos = createAdditionalInfos();
         try {
+
             return visualizationProcessor
-                    .visualize(requestDTO.getRootSnapshotId(), settings, requestDTO, snapshotTreeResult);
+                    .visualize(LayoutViewType.CITY, settings, snapshotTreeResult, additionalInfos);
         } catch (DotExecutorException e) {
             throw new Exception(e.getMessage(), e);
         }
     }
 
-    private VisualizationRequest createExampleVisualizationRequest() {
-        return new VisualizationRequest(1, LayoutViewType.CITY, 1, 20);
+    private VisualizationAdditionalInfos createAdditionalInfos() {
+
+        MinMaxValue minMaxFootprint = new MinMaxValue(0, 3);
+        MinMaxValue minMaxHeight = new MinMaxValue(0, 3);
+        MinMaxValue minMaxColor = new MinMaxValue(0, 3);
+        int dependencyCount = 0;
+
+        return new VisualizationAdditionalInfos(minMaxFootprint, minMaxHeight, minMaxColor, dependencyCount);
     }
 
-    private SnapshotTreeResult createSnapShotTreeResult(VisualizationRequest requestDTO ) {
-        final RootTreeNode result = new RootTreeNode(requestDTO.getRootSnapshotId());
+    private SnapshotTreeResult createSnapShotTreeResult(final int rootId) {
+        final RootTreeNode result = new RootTreeNode(rootId);
 
         TreeNode node2 = new ValueTreeNode(2, result, 1, TreeNodeType.TREE, "2", 1.3, 1.5, 2);
         TreeNode node3 = new ValueTreeNode(3, result, 1, TreeNodeType.TREE, "3", 1.7, 1.8, 1);
         result.addChildrenNode("2", node2);
         result.addChildrenNode("3", node3);
 
-        SnapshotStorageKey key = new SnapshotStorageKey(requestDTO);
-        return new SnapshotTreeResult(key, result);
+        return new SnapshotTreeResult(result);
     }
 }
