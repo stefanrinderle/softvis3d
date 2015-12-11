95c07ad170d9e2329947b6049f7b00ecba1f1e8e
Stefan Rinderle <stefan@rinderle.info>
2015-12-11 14:58:22 -0600

    First cleanup

--- src/main/java/de/rinderle/softvis3d/servlet/SoftVis3DConfig.java
+++ src/main/java/de/rinderle/softvis3d/servlet/SoftVis3DConfig.java
@@ -2,6 +2,7 @@ package de.rinderle.softvis3d.servlet;
 
 import com.sun.jersey.guice.JerseyServletModule;
 import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
+import de.rinderle.softvis3d.guice.SoftVis3DModule;
 
 public class SoftVis3DConfig extends JerseyServletModule {
 
