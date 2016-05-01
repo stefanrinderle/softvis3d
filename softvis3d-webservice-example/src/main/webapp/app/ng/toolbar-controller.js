e97bbc0b757f85087037aca09918af20e368e612
Stefan Rinderle <stefan@rinderle.info>
2016-05-01 11:14:03 +0200

    Cleanup edge logic in webservice example

--- softvis3d-webservice-example/src/main/webapp/app/ng/toolbar-controller.js
+++ softvis3d-webservice-example/src/main/webapp/app/ng/toolbar-controller.js
@@ -36,14 +36,6 @@ ThreeViewer.ToolbarController = function ($scope, ViewerService, TreeService, Me
   this.node = null;
   this.displayChildren = false;
 
-  this.inEdges = [];
-  this.outEdges = [];
-  this.displayEdges = false;
-
-  this.edge = null;
-  this.edgeIncludingEdges = null;
-  this.displayEdgeIncludingEdges = false;
-
   this.metric1Name = "";
   this.metric2Name = "";
   this.scmMetricName = "";
@@ -80,104 +72,11 @@ ThreeViewer.ToolbarController.prototype.listeners = function () {
 
 };
 
-ThreeViewer.ToolbarController.prototype.showDetails = function (softVis3dId, type) {
-  if (type === "dependency") {
-    this.node = null;
-    this.edge = this.TreeService.searchEdge(softVis3dId);
-    this.privateShowEdgeEdgeDetails(this.edge);
-    this.inEdges = [];
-    this.outEdges = [];
-  } else {
+ThreeViewer.ToolbarController.prototype.showDetails = function (softVis3dId) {
     this.node = this.TreeService.searchTreeNode(softVis3dId);
-    this.privateShowNodeEdgeDetails(this.node);
-    this.edge = null;
-    this.edgeIncludingEdges = null;
-    this.displayEdgeIncludingEdges = false;
-  }
-};
-
-ThreeViewer.ToolbarController.prototype.privateShowNodeEdgeDetails = function (sourceNode) {
-  this.outEdges = [];
-  // outbound dependencies
-  for (var index = 0; index < sourceNode.edges.length; index++) {
-    var sourceEdge = sourceNode.edges[index];
-    var targetEdge = {};
-    targetEdge.id = sourceEdge.id;
-    targetEdge.sourceName = sourceEdge.sourceName;
-    targetEdge.destinationName = sourceEdge.destinationName;
-    // copy including dependencies
-    targetEdge.includingDependencies = sourceEdge.includingDependencies.slice();
-
-    for (var j = 0; j < targetEdge.includingDependencies.length; j++) {
-      targetEdge.includingDependencies[j].displayValue =
-          this.TreeService.getDependencyNameForId(targetEdge.includingDependencies[j].id);
-    }
-
-    this.outEdges.push(targetEdge);
-  }
-
-  this.inEdges = [];
-  // inbound dependencies
-  var inboundEdges = this.TreeService.getInboundEdges(sourceNode);
-  for (var inboundIndex = 0; inboundIndex < inboundEdges.length; inboundIndex++) {
-    sourceEdge = inboundEdges[inboundIndex];
-    targetEdge = {};
-    targetEdge.id = sourceEdge.id;
-    targetEdge.sourceName = sourceEdge.sourceName;
-    targetEdge.destinationName = sourceEdge.destinationName;
-    // copy including dependencies
-    targetEdge.includingDependencies = sourceEdge.includingDependencies.slice();
-
-    for (var includingIndex = 0; includingIndex < targetEdge.includingDependencies.length; includingIndex++) {
-      targetEdge.includingDependencies[includingIndex].displayValue =
-          this.TreeService.getDependencyNameForId(targetEdge.includingDependencies[includingIndex].id);
-    }
-
-    this.inEdges.push(targetEdge);
-  }
-};
-
-ThreeViewer.ToolbarController.prototype.privateShowEdgeEdgeDetails = function (sourceEdge) {
-  this.edgeIncludingEdges = [];
-
-  for (var j = 0; j < sourceEdge.includingDependencies.length; j++) {
-    var result = {};
-    result.id = sourceEdge.includingDependencies[j].id;
-    result.displayValue = this.TreeService.getDependencyNameForId(sourceEdge.includingDependencies[j].id);
-
-    this.edgeIncludingEdges.push(result);
-  }
-};
-
-ThreeViewer.ToolbarController.prototype.selectAllDependentDependencies = function (edge) {
-  var clearedIncludingDependencyIds = [];
-  for (var index = 0; index < edge.includingDependencies.length; index++) {
-    clearedIncludingDependencyIds.push(edge.includingDependencies[index].id);
-  }
-
-  this.selectAllDependentDependenciesByIds(clearedIncludingDependencyIds);
-};
-
-ThreeViewer.ToolbarController.prototype.selectAllDependentDependenciesById = function (dependencyId) {
-  var ids = [];
-  ids.push(dependencyId);
-  this.selectAllDependentDependenciesByIds(ids);
-};
-
-ThreeViewer.ToolbarController.prototype.selectAllDependentDependenciesByIds = function (includingDependencyIds) {
-  var edgeIds = this.TreeService.getAllDependentEdgeIds(includingDependencyIds);
-  this.ViewerService.selectSceneEdgeObjects(edgeIds);
 };
 
 ThreeViewer.ToolbarController.prototype.selectSceneObjectFromDetails = function (objectId, type) {
-  if (type === "dependency") {
-    var edgeIds = [];
-    edgeIds.push(objectId);
-    this.ViewerService.selectSceneEdgeObjects(edgeIds);
-  } else {
-    this.ViewerService.selectSceneTreeObject(objectId);
-  }
-
   this.showDetails(objectId, type);
 };
 
@@ -196,29 +95,5 @@ ThreeViewer.ToolbarController.prototype.triggerDisplayChildren = function () {
     this.displayChildren = false;
   } else {
     this.displayChildren = true;
-  }
-};
-
-ThreeViewer.ToolbarController.prototype.triggerDisplayEdges = function () {
-  if (this.displayEdges) {
-    this.displayEdges = false;
-  } else {
-    this.displayEdges = true;
-  }
-};
-
-ThreeViewer.ToolbarController.prototype.triggerDisplayEdgeDetails = function (edge) {
-  if (edge.displayDetails) {
-    edge.displayDetails = false;
-  } else {
-    edge.displayDetails = true;
-  }
-};
-
-ThreeViewer.ToolbarController.prototype.triggerDisplayEdgeIncludingEdges = function () {
-  if (this.displayEdgeIncludingEdges) {
-    this.displayEdgeIncludingEdges = false;
-  } else {
-    this.displayEdgeIncludingEdges = true;
   }
 };
