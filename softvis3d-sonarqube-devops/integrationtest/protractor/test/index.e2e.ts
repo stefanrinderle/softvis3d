fa122d0d91b6e738d44dfd8d8f12dc00d6405b6e
Stefan Rinderle <stefan@rinderle.info>
2016-07-14 23:14:46 +0200

    #29 Tests for start page of the plugin and default visualization.

--- softvis3d-sonarqube-devops/integrationtest/protractor/test/index.e2e.ts
+++ softvis3d-sonarqube-devops/integrationtest/protractor/test/index.e2e.ts
@@ -1,16 +1,41 @@
 ///<reference path="../typings/jasmine/jasmine.d.ts"/>
 ///<reference path="../typings/angular-protractor/angular-protractor.d.ts"/>
 
+function waitFor(selector) {
+    return browser.wait(function() {
+        return browser.isElementPresent(selector);
+    }, 5000);
+}
+
 describe('Index', () => {
 
     beforeEach(() => {
-        browser.get('/');
+        browser.get('/sonar');
+
+        element(by.linkText('softvis3d')).click();
+        browser.sleep(2000);
+        element.all(by.partialLinkText('More')).get(1).click();
+        element.all(by.partialLinkText('SoftVis3D Viewer')).click();
+
+        waitFor(by.buttonText('Show'));
     });
 
-    it('should have a title', () => {
-        let subject = browser.getTitle();
-        let result = 'SonarQube';
-        expect(subject).toEqual(result);
+    it('shoud show the menu', () => {
+
+        var showbutton = element(by.buttonText('Show'));
+
+        expect(showbutton.isEnabled()).toBe(true);
+        expect(element(by.id('file-loader')).isDisplayed()).toBe(true);
+        browser.sleep(2000);
+    });
+
+    it('shoud show the visualization', () => {
+
+        var showbutton = element(by.buttonText('Show'));
+        showbutton.click();
+
+        expect(element(by.id('file-loader')).isDisplayed()).toBe(false);
+        browser.sleep(2000);
     });
 
 });
