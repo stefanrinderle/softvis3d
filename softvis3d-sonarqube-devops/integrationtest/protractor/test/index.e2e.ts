85510f7414f1f5c982a1eba41be78e2dd7a4a0e4
Stefan Rinderle <stefan@rinderle.info>
2016-08-21 19:41:00 +0200

    #30 Adjust integration test

--- softvis3d-sonarqube-devops/integrationtest/protractor/test/index.e2e.ts
+++ softvis3d-sonarqube-devops/integrationtest/protractor/test/index.e2e.ts
@@ -7,75 +7,73 @@ function waitFor(selector) {
     }, 5000);
 }
 
-describe('Index', () => {
+describe("Index", () => {
 
     beforeEach(() => {
-        browser.get('/');
+        browser.get("/");
 
-        element(by.linkText('softvis3d')).click();
+        element(by.linkText("softvis3d")).click();
         browser.sleep(2000);
-        element.all(by.partialLinkText('More')).get(1).click();
-        element.all(by.partialLinkText('SoftVis3D Viewer')).click();
+        element.all(by.partialLinkText("More")).get(1).click();
+        element.all(by.partialLinkText("SoftVis3D Viewer")).click();
 
-        waitFor(by.buttonText('Show'));
+        waitFor(by.buttonText("Show"));
     });
 
-    it('shoud show the menu', () => {
-        var showbutton = element(by.id('cityButtonShow'));
+    it("shoud show the menu", () => {
+        let showbutton = element(by.id("cityButtonShow"));
 
         expect(showbutton.isEnabled()).toBe(true);
-        expect(element(by.id('file-loader')).isDisplayed()).toBe(true);
+        expect(element(by.id("file-loader")).isDisplayed()).toBe(true);
         browser.sleep(2000);
     });
 
-    it('shoud show the default visualization', () => {
-        var showbutton = element(by.id('cityButtonShow'));
+    it("shoud show the default visualization", () => {
+        let showbutton = element(by.id("cityButtonShow"));
         showbutton.click();
 
         browser.sleep(2000);
 
-        element(by.id('extendTreeImage')).click();
-        element(by.linkText('softvis3d-base')).click();
+        element(by.id("extendTreeImage")).click();
     });
 
-    it('shoud show the custom menu', () => {
-        var customButton = element(by.id('navigationButtonCustom'));
+    it("shoud show the custom menu", () => {
+        let customButton = element(by.id("navigationButtonCustom"));
         customButton.click();
     });
 
-    it('shoud show the custom default visualization', () => {
-        var customButton = element(by.id('navigationButtonCustom'));
+    it("shoud show the custom default visualization", () => {
+        let customButton = element(by.id("navigationButtonCustom"));
         customButton.click();
 
-        var showbutton = element(by.id('customButtonShow'));
+        let showbutton = element(by.id("customButtonShow"));
         showbutton.click();
 
         browser.sleep(2000);
     });
 
-    it('shoud show the custom evostreet visualization', () => {
-        var custombutton = element(by.id('navigationButtonCustom'));
+    it("shoud show the custom evostreet visualization", () => {
+        let custombutton = element(by.id("navigationButtonCustom"));
         custombutton.click();
 
-        element(by.cssContainingText('option', 'Evostreet')).click();
+        element(by.cssContainingText("option", "Evostreet")).click();
 
-        var showbutton = element(by.id('customButtonShow'));
+        let showbutton = element(by.id("customButtonShow"));
         showbutton.click();
 
         browser.sleep(2000);
     });
 
-    it('shoud show custom visualization with colors', () => {
-        var custombutton = element(by.id('navigationButtonCustom'));
+    it("shoud show custom visualization with colors", () => {
+        let custombutton = element(by.id("navigationButtonCustom"));
         custombutton.click();
 
-        element(by.cssContainingText('option', 'Package Name')).click();
+        element(by.cssContainingText("option", "Package Name")).click();
 
-        var showbutton = element(by.id('customButtonShow'));
+        let showbutton = element(by.id("customButtonShow"));
         showbutton.click();
 
         browser.sleep(2000);
     });
-
 
 });
