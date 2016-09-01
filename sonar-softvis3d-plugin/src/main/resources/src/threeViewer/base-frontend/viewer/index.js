5dae1abe89397b15f575549c8f42aa3dd4e4911d
Stefan Rinderle <stefan@rinderle.info>
2016-09-01 20:57:58 +0200

    #30 Removed not longer used NURBS js files.

--- sonar-softvis3d-plugin/src/main/resources/src/threeViewer/base-frontend/viewer/index.js
+++ sonar-softvis3d-plugin/src/main/resources/src/threeViewer/base-frontend/viewer/index.js
@@ -17,8 +17,6 @@
  * License along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
  */
-require('./lib/NURBSCurve.js');
-require('./lib/NURBSUtils.js');
 
 var Viewer = require('./viewer.js');
 
