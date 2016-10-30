77e9085e8ced722269c1040614ede1fd53d4b6a0
Yvo Niedrich <yvo.niedrich@gmail.com>
2016-10-30 12:18:21 +0100

    reduced branch complexity and updated tests (webgl)

--- softvis3d-frontend/src/main/resources/src/components/scene/webgl/WebGLDetector.ts
+++ softvis3d-frontend/src/main/resources/src/components/scene/webgl/WebGLDetector.ts
@@ -23,22 +23,22 @@ export class WebGLDetector {
     public static isWebGLSupported(): boolean {
         try {
             let canvas = document.createElement("canvas");
-            return !!( (<any> window).WebGLRenderingContext
-            && ( canvas.getContext("webgl") || canvas.getContext("experimental-webgl") ) );
+            return !!((window as any).WebGLRenderingContext
+                && (canvas.getContext("webgl") || canvas.getContext("experimental-webgl")));
         } catch (e) {
             return false;
         }
     }
 
-    public static getWebGLErrorMessage() {
-        if (!this.isWebGLSupported()) {
-            if ((<any> window).WebGLRenderingContext) {
-                return "Your graphics card does not seem to support WebGL. Find out how to get it on http://get.webgl.org/";
-            } else {
-                return "Your browser does not seem to support WebGL. Find out how to get it on http://get.webgl.org/.";
-            }
-        } else {
+    public static getWebGLErrorMessage(): string {
+        if (this.isWebGLSupported()) {
             return "";
+        }
+
+        if (typeof window !== "undefined" && (window as any).WebGLRenderingContext) {
+            return "Your graphics card does not seem to support WebGL. Find out how to get it on http://get.webgl.org/";
+        } else {
+            return "Your browser does not seem to support WebGL. Find out how to get it on http://get.webgl.org/.";
         }
     }
 
