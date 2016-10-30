e7309b7d5b27aca73b8c66faa6cfea9dac96f0e6
Stefan Rinderle <stefan@rinderle.info>
2016-10-30 19:11:43 +0100

    #20 Optimize sonarqube analysis results. Add lcov.dat for typescript module.

--- softvis3d-frontend/src/main/resources/test/components/scene/webgl/WebGLDetector.spec.ts
+++ softvis3d-frontend/src/main/resources/test/components/scene/webgl/WebGLDetector.spec.ts
@@ -25,7 +25,7 @@ describe("WebGLDetector", () => {
 
     it("webGL is not supported without mocks", () => {
         expect(WebGLDetector.isWebGLSupported()).to.be.equal(false);
-        expect(WebGLDetector.getWebGLErrorMessage()).to.contain("http://get.webgl.org/")
+        expect(WebGLDetector.getWebGLErrorMessage()).to.contain("http://get.webgl.org/");
     });
 
 });
