3d850fbad362375695bee7feb35791f0e6135800
Yvo Niedrich <yvo.niedrich@gmail.com>
2016-11-01 16:06:15 +0100

    use three as external library

--- softvis3d-frontend/src/main/resources/webpack.config.js
+++ softvis3d-frontend/src/main/resources/webpack.config.js
@@ -83,7 +83,8 @@ module.exports = [
         // dependencies, which allows browsers to cache those libraries between builds.
         externals: {
             "react": "React",
-            "react-dom": "ReactDOM"
+            "react-dom": "ReactDOM",
+            "three": "THREE"
         },
         devServer: {
 
