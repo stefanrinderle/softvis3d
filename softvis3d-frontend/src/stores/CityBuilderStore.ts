ebff0b352378011bf4c08f3a61df11219c08a6ed
Yvo Niedrich <yvo.niedrich@gmail.com>
2016-12-04 21:26:24 +0100

    initialize metric values again

--- softvis3d-frontend/src/stores/CityBuilderStore.ts
+++ softvis3d-frontend/src/stores/CityBuilderStore.ts
@@ -2,6 +2,7 @@ import {observable, computed} from "mobx";
 import {district} from "../dtos/Layouts";
 import {demo} from "../dtos/Profiles";
 import appStatusStore from "./AppStatusStore";
+import {noMetric} from "../dtos/Metrics";
 
 class CityBuilderStore {
     @observable public layoutType: Layout;
@@ -16,14 +17,13 @@ class CityBuilderStore {
     public constructor() {
         this.show = false;
         this.layoutType = district;
+        this.metricColor = noMetric;
+        this.metricHeight = noMetric;
+        this.metricWidth = noMetric;
         this.setProfile(demo);
 
         this.availableMetrics = observable([]);
-        this.availableMetrics.push({
-            key: "NONE",
-            type: "NONE",
-            name: " -- None -- "
-        });
+        this.availableMetrics.push(noMetric);
     }
 
     @computed public get isVisible() {
