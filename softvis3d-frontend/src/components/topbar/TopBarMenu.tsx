87e2a46e8fcd9bdde04b6ff165478d36b7f2df07
Yvo Niedrich <yvo.niedrich@gmail.com>
2017-01-18 23:24:43 +0100

    add help-button to top bar

--- softvis3d-frontend/src/components/topbar/TopBarMenu.tsx
+++ softvis3d-frontend/src/components/topbar/TopBarMenu.tsx
@@ -6,18 +6,22 @@ import {CityBuilderStore} from "../../stores/CityBuilderStore";
  * Currently used for an example use of selected scene object store.
  */
 @observer
-export default class TopBarMenu extends React.Component<{ cityBuilderStore: CityBuilderStore}, any> {
+export default class TopBarMenu extends React.Component<{ cityBuilderStore: CityBuilderStore }, any> {
 
     public render() {
         return (
             <div className="top-bar-menu">
-                <button onClick={this.showSettings.bind(this)}>Settings</button>
+                <button className="left" onClick={this.showBuilder.bind(this)}>Settings</button>
+                <button className="right" onClick={this.showSettings.bind(this)}>Help</button>
             </div>
         );
     }
 
-    private showSettings() {
+    private showBuilder() {
         this.props.cityBuilderStore.show = true;
     }
 
+    private showSettings() {
+        window.open("http://softvis3d.com/#/details/usage");
+    }
 }
