b36d5f5c586f0d0c8d667c29eb3aaca093f3a263
Stefan Rinderle <stefan@rinderle.info>
2017-03-08 14:17:14 +0100

    #47 Removed base component and shifted the class to the plugin module - build issue and license update.

--- sonar-softvis3d-plugin/src/main/java/de/rinderle/softvis3d/webservice/TreeNodeJsonWriter.java
+++ sonar-softvis3d-plugin/src/main/java/de/rinderle/softvis3d/webservice/TreeNodeJsonWriter.java
@@ -1,7 +1,7 @@
-/*
- * softvis3d-base
- * Copyright (C) 2015 Stefan Rinderle
- * stefan@rinderle.info
+/**
+ * SoftVis3D Sonar plugin
+ * Copyright (C) 2016 Stefan Rinderle and Yvo Niedrich
+ * stefan@rinderle.info / yvo.niedrich@gmail.com
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
