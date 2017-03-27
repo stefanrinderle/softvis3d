d1c07178295247b23727810b248b8f9de17132fb
Stefan Rinderle <stefan@rinderle.info>
2017-03-27 21:35:04 +0200

    Cleanup a bit

--- sonar-softvis3d-plugin/src/main/resources/static/overview_page.js
+++ sonar-softvis3d-plugin/src/main/resources/static/overview_page.js
@@ -1,3 +1,21 @@
+window.registerExtension('softvis3d/overview_page', function (options) {
+
+    window.PROJECT_KEY = options.component.key;
+
+    let s = '<div id="app"></div>';
+    let div = document.createElement('div');
+    div.innerHTML = s;
+    options.el.appendChild(div);
+
+    loadStyle('/static/softvis3d/style.css');
+
+    loadScript('/static/softvis3d/react.min.js', function () {});
+    loadScript('/static/softvis3d/react-dom.min.js', function () {});
+    loadScript('/static/softvis3d/three.min.js', function () {
+        loadScript('/static/softvis3d/bundle.js');
+    });
+});
+
 function loadScript(src, f) {
     let head = document.getElementsByTagName("head")[0];
     let script = document.createElement("script");
@@ -38,31 +56,3 @@ function loadStyle(src) {
 
     head.appendChild(script);
 }
-
-window.registerExtension('softvis3d/overview_page', function (options) {
-    // let's create a flag telling if the page is still displayed
-    let isDisplayed = true;
-
-    window.PROJECT_KEY = options.component.key;
-
-    loadStyle('/static/softvis3d/style.css');
-
-    let s = '<div id="app"></div>';
-    let div = document.createElement('div');
-    div.innerHTML = s;
-    options.el.appendChild(div);
-
-    loadScript('/static/softvis3d/react.min.js', function () {});
-    loadScript('/static/softvis3d/react-dom.min.js', function () {});
-    loadScript('/static/softvis3d/three.min.js', function () {
-        loadScript('/static/softvis3d/bundle.js', function () {
-            console.log('finished loading');
-        });
-    });
-
-    // return a function, which is called when the page is being closed
-    return function () {
-        // we unset the `isDisplayed` flag to ignore to Web API calls finished after the page is closed
-        isDisplayed = false;
-    };
-});
