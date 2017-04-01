cd467b71a9b83a557c7d8dea7941d7a703453d6d
Yvo Niedrich <yvo.niedrich@gmail.com>
2017-04-01 19:31:05 +0200

    #65: Update scale description

--- softvis3d-frontend/src/constants/Scales.ts
+++ softvis3d-frontend/src/constants/Scales.ts
@@ -2,16 +2,15 @@ import Scale from "../classes/Scale";
 
 const LOGARITHMIC: Scale = new Scale("logarithmic", "Logarithmic",
     "Building footprint and height values are calculated using a logarithmic scale " +
-    "based the metric min and max values.");
+    "based the projects most degenerate file");
 const EXPONENTIAL: Scale = new Scale("exponential", "Exponential",
     "Building footprint and height values are calculated using a exponential scale " +
-    "based the metric min and max values.");
+    "based the projects most degenerate file");
 const LINEAR_SCALED: Scale = new Scale("linear_s", "Linear (scaled)",
-    "Building footprint and height will be scaled linear without a maximum value. " +
-    "The max footprint and height is dependent on the metric values.");
+    "Building footprint and height will be scaled linearly. If a file's metric value exceeds " +
+    " a maximum value the whole project will be scaled down.");
 const LINEAR: Scale = new Scale("linear", "Linear",
-    "Building footprint and height will be scaled linear with a maximum value defined by the plugin. " +
-    "The max footprint and height is fixed.");
+    "Building footprint and height will be scaled linearly without a maximum value.");
 
 export {
     LOGARITHMIC,
