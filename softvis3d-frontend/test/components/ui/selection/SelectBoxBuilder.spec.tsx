3bbfc55dd3b3395ab02e8196a7b50067d8dd49eb
Stefan Rinderle <stefan@rinderle.info>
2017-04-27 19:34:22 +0200

    Check test file folders recursive.

--- softvis3d-frontend/test/components/ui/selection/SelectBoxBuilder.spec.tsx
+++ softvis3d-frontend/test/components/ui/selection/SelectBoxBuilder.spec.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 import * as Sinon from "sinon";
-import {assert, expect} from "chai";
-import {mount} from "enzyme";
-import SelectBoxBuilder from "../../../src/components/ui/selectbox/SelectBoxBuilder";
+import { assert, expect } from "chai";
+import { mount } from "enzyme";
+import SelectBoxBuilder from "../../../../src/components/ui/selectbox/SelectBoxBuilder";
 
 describe("<SelectBoxBuilder/>", () => {
 
