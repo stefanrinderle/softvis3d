02df0c53512c27ba5c2c3a5e21658a14b7c9c023
Stefan Rinderle <stefan@rinderle.info>
2017-05-13 12:47:36 +0200

    let should be vra to support all browser versions.

--- sonar-softvis3d-plugin/src/main/resources/static/overview_page.js
+++ sonar-softvis3d-plugin/src/main/resources/static/overview_page.js
@@ -1,23 +1,23 @@
 // Promise polyfill: https://github.com/taylorhakes/promise-polyfill
 if (typeof(Promise) === "undefined") {
-    !function(e){function n(){}function t(e,n){return function(){e.apply(n,arguments)}}function o(e){if("object"!=typeof this)throw new TypeError("Promises must be constructed via new");if("function"!=typeof e)throw new TypeError("not a function");this._state=0,this._handled=!1,this._value=void 0,this._deferreds=[],s(e,this)}function i(e,n){for(;3===e._state;)e=e._value;return 0===e._state?void e._deferreds.push(n):(e._handled=!0,void o._immediateFn(function(){let t=1===e._state?n.onFulfilled:n.onRejected;if(null===t)return void(1===e._state?r:u)(n.promise,e._value);let o;try{o=t(e._value)}catch(i){return void u(n.promise,i)}r(n.promise,o)}))}function r(e,n){try{if(n===e)throw new TypeError("A promise cannot be resolved with itself.");if(n&&("object"==typeof n||"function"==typeof n)){let i=n.then;if(n instanceof o)return e._state=3,e._value=n,void f(e);if("function"==typeof i)return void s(t(i,n),e)}e._state=1,e._value=n,f(e)}catch(r){u(e,r)}}function u(e,n){e._state=2,e._value=n,f(e)}function f(e){2===e._state&&0===e._deferreds.length&&o._immediateFn(function(){e._handled||o._unhandledRejectionFn(e._value)});for(let n=0,t=e._deferreds.length;n<t;n++)i(e,e._deferreds[n]);e._deferreds=null}function c(e,n,t){this.onFulfilled="function"==typeof e?e:null,this.onRejected="function"==typeof n?n:null,this.promise=t}function s(e,n){let t=!1;try{e(function(e){t||(t=!0,r(n,e))},function(e){t||(t=!0,u(n,e))})}catch(o){if(t)return;t=!0,u(n,o)}}let a=setTimeout;o.prototype["catch"]=function(e){return this.then(null,e)},o.prototype.then=function(e,t){let o=new this.constructor(n);return i(this,new c(e,t,o)),o},o.all=function(e){let n=Array.prototype.slice.call(e);return new o(function(e,t){function o(r,u){try{if(u&&("object"==typeof u||"function"==typeof u)){let f=u.then;if("function"==typeof f)return void f.call(u,function(e){o(r,e)},t)}n[r]=u,0===--i&&e(n)}catch(c){t(c)}}if(0===n.length)return e([]);for(let i=n.length,r=0;r<n.length;r++)o(r,n[r])})},o.resolve=function(e){return e&&"object"==typeof e&&e.constructor===o?e:new o(function(n){n(e)})},o.reject=function(e){return new o(function(n,t){t(e)})},o.race=function(e){return new o(function(n,t){for(let o=0,i=e.length;o<i;o++)e[o].then(n,t)})},o._immediateFn="function"==typeof setImmediate&&function(e){setImmediate(e)}||function(e){a(e,0)},o._unhandledRejectionFn=function(e){"undefined"!=typeof console&&console&&console.warn("Possible Unhandled Promise Rejection:",e)},o._setImmediateFn=function(e){o._immediateFn=e},o._setUnhandledRejectionFn=function(e){o._unhandledRejectionFn=e},"undefined"!=typeof module&&module.exports?module.exports=o:e.Promise||(e.Promise=o)}(this);
+    !function(e){function n(){}function t(e,n){return function(){e.apply(n,arguments)}}function o(e){if("object"!=typeof this)throw new TypeError("Promises must be constructed via new");if("function"!=typeof e)throw new TypeError("not a function");this._state=0,this._handled=!1,this._value=void 0,this._deferreds=[],s(e,this)}function i(e,n){for(;3===e._state;)e=e._value;return 0===e._state?void e._deferreds.push(n):(e._handled=!0,void o._immediateFn(function(){var t=1===e._state?n.onFulfilled:n.onRejected;if(null===t)return void(1===e._state?r:u)(n.promise,e._value);var o;try{o=t(e._value)}catch(i){return void u(n.promise,i)}r(n.promise,o)}))}function r(e,n){try{if(n===e)throw new TypeError("A promise cannot be resolved with itself.");if(n&&("object"==typeof n||"function"==typeof n)){var i=n.then;if(n instanceof o)return e._state=3,e._value=n,void f(e);if("function"==typeof i)return void s(t(i,n),e)}e._state=1,e._value=n,f(e)}catch(r){u(e,r)}}function u(e,n){e._state=2,e._value=n,f(e)}function f(e){2===e._state&&0===e._deferreds.length&&o._immediateFn(function(){e._handled||o._unhandledRejectionFn(e._value)});for(var n=0,t=e._deferreds.length;n<t;n++)i(e,e._deferreds[n]);e._deferreds=null}function c(e,n,t){this.onFulfilled="function"==typeof e?e:null,this.onRejected="function"==typeof n?n:null,this.promise=t}function s(e,n){var t=!1;try{e(function(e){t||(t=!0,r(n,e))},function(e){t||(t=!0,u(n,e))})}catch(o){if(t)return;t=!0,u(n,o)}}var a=setTimeout;o.prototype["catch"]=function(e){return this.then(null,e)},o.prototype.then=function(e,t){var o=new this.constructor(n);return i(this,new c(e,t,o)),o},o.all=function(e){var n=Array.prototype.slice.call(e);return new o(function(e,t){function o(r,u){try{if(u&&("object"==typeof u||"function"==typeof u)){var f=u.then;if("function"==typeof f)return void f.call(u,function(e){o(r,e)},t)}n[r]=u,0===--i&&e(n)}catch(c){t(c)}}if(0===n.length)return e([]);for(var i=n.length,r=0;r<n.length;r++)o(r,n[r])})},o.resolve=function(e){return e&&"object"==typeof e&&e.constructor===o?e:new o(function(n){n(e)})},o.reject=function(e){return new o(function(n,t){t(e)})},o.race=function(e){return new o(function(n,t){for(var o=0,i=e.length;o<i;o++)e[o].then(n,t)})},o._immediateFn="function"==typeof setImmediate&&function(e){setImmediate(e)}||function(e){a(e,0)},o._unhandledRejectionFn=function(e){"undefined"!=typeof console&&console&&console.warn("Possible Unhandled Promise Rejection:",e)},o._setImmediateFn=function(e){o._immediateFn=e},o._setUnhandledRejectionFn=function(e){o._unhandledRejectionFn=e},"undefined"!=typeof module&&module.exports?module.exports=o:e.Promise||(e.Promise=o)}(this);
 }
 
 (function (entryPoint, baseUrl) {
     function createEntryPoint(target, name) {
-        let div = document.createElement("div");
+        var div = document.createElement("div");
         div.setAttribute("id", name);
         target.appendChild(div);
     }
 
     function loadScript(src, f) {
-        let head = document.getElementsByTagName("head")[0];
-        let script = document.createElement("script");
+        var head = document.getElementsByTagName("head")[0];
+        var script = document.createElement("script");
         script.src = src;
-        let done = false;
+        var done = false;
         script.onload = script.onreadystatechange = function () {
             // attach to both events for cross browser finish detection:
-            if (!done && (!this.readyState || this.readyState === "loaded" || this.readyState === "complete")) {
+            if (!done && (!this.readyState || this.readyState === "loaded" || this.readyState === "compvare")) {
                 done = true;
                 if (typeof f === "function") {
                     f();
@@ -31,17 +31,17 @@ if (typeof(Promise) === "undefined") {
     }
 
     function loadStyle(src, f) {
-        let head = document.getElementsByTagName("head")[0];
-        let script = document.createElement("link");
+        var head = document.getElementsByTagName("head")[0];
+        var script = document.createElement("link");
         script.setAttribute("rel", "stylesheet");
         script.setAttribute("type", "text/css");
         script.setAttribute("href", src);
 
         script.src = src;
-        let done = false;
+        var done = false;
         script.onload = script.onreadystatechange = function () {
             // attach to both events for cross browser finish detection:
-            if (!done && (!this.readyState || this.readyState === "loaded" || this.readyState === "complete")) {
+            if (!done && (!this.readyState || this.readyState === "loaded" || this.readyState === "compvare")) {
                 done = true;
                 if (typeof f === "function") {
                     f();
@@ -55,18 +55,18 @@ if (typeof(Promise) === "undefined") {
     }
 
     function load() {
-        let completed = 0;
-        let limit = arguments.length;
-        let files = arguments;
+        var compvared = 0;
+        var limit = arguments.length;
+        var files = arguments;
 
         return new Promise(function(resolve) {
-            let fn = function() {
-                if (++completed === limit) {
+            var fn = function() {
+                if (++compvared === limit) {
                     resolve();
                 }
             };
 
-            for (let i = 0; i < files.length; i++) {
+            for (var i = 0; i < files.length; i++) {
                 loader = files[i].match(/css$/i) ? loadStyle : loadScript;
                 loader(files[i], fn);
             }
@@ -87,7 +87,7 @@ if (typeof(Promise) === "undefined") {
             baseUrl + "/static/softvis3d/three.min.js"
         ).then(function() {
             load(baseUrl + "/static/softvis3d/bundle.js").then(function() {
-                let softvis3d = new window.softvis3d.app({
+                var softvis3d = new window.softvis3d.app({
                     api: baseUrl + "/api",
                     projectKey: options.component.key,
                     isDev: false
