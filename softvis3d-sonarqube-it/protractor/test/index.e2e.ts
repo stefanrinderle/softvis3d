ed97fec72366881934b9fdabd0683a1d00dc357c
Stefan Rinderle <stefan@rinderle.info>
2017-05-13 15:02:32 +0200

    fixed e2e integration tests.

--- softvis3d-sonarqube-it/protractor/test/index.e2e.ts
+++ softvis3d-sonarqube-it/protractor/test/index.e2e.ts
@@ -7,19 +7,6 @@ function waitFor(selector) {
     }, 10000);
 }
 
-function checkConsoleErrors() {
-    browser.manage().logs().get('browser').then((browserLog) => {
-        if (browserLog.length) {
-            browserLog.forEach((log) => {
-                let error = log.level.value > 900;
-                if (error) {
-                    console.error('ERROR: ', JSON.stringify(browserLog));
-                }
-            });
-        }
-    });
-}
-
 describe("Index", () => {
 
     beforeEach(() => {
@@ -30,94 +17,78 @@ describe("Index", () => {
         waitFor(by.css(".overview"));
 
         element.all(by.partialLinkText("More")).get(0).click();
-
-    });
-
-    // afterEach(() => {
-    //     checkConsoleErrors();
-    // });
-
-    // it("shoud show default page 1", () => {
-    //
-    // });
-
-    it("shoud show default page 2", () => {
-        checkConsoleErrors();
-
         element(by.partialLinkText("SoftVis3D Viewer")).click();
 
-        checkConsoleErrors();
-
-        browser.sleep(3000);
-
-        checkConsoleErrors();
+        waitFor(by.css(".city-builder"));
     });
 
-    // it("shoud show default page 3", () => {
-    //     browser.get("/project/extension/softvis3d/overview_page?id=de.rinderle.softvis3d%3Asoftvis3d");
-    //
-    //     checkConsoleErrors();
-    // });
+    it("shoud show the main menu default", () => {
+        expect(element(by.css(".city-builder")).isDisplayed()).toBe(true);
+        browser.sleep(2000);
+    });
 
-    // it("shoud show the main menu default", () => {
-    //     expect(element(by.css(".city-builder")).isDisplayed()).toBe(true);
-    //     browser.sleep(2000);
-    // });
-    //
-    // it("shoud show the District option", () => {
-    //     element(by.id("select-district")).click();
-    //     element(by.partialButtonText("Load Scene")).click();
-    //     browser.sleep(2000);
-    //
-    //     browser.actions()
-    //         .mouseMove({x: 300, y: 300})
-    //         .mouseDown()
-    //         .mouseUp()
-    //         .perform();
-    //
-    //     browser.sleep(500);
-    //
-    //     element(by.cssContainingText("option", "Package Name")).click();
-    //
-    //     browser.sleep(2000);
-    // });
-    //
-    // it("shoud show the District option with the leak period profile", () => {
-    //     element(by.id("select-district")).click();
-    //     element(by.cssContainingText("option", "Leak period")).click();
-    //
-    //     element(by.partialButtonText("Load Scene")).click();
-    //     browser.sleep(2000);
-    //
-    //     browser.actions()
-    //         .mouseMove({x: 300, y: 300})
-    //         .mouseDown()
-    //         .mouseUp()
-    //         .perform();
-    //
-    //     browser.sleep(500);
-    //
-    //     element(by.cssContainingText("option", "Package Name")).click();
-    //
-    //     browser.sleep(2000);
-    // });
-    //
-    // it("shoud show the evostreet option", () => {
-    //     element(by.id("select-evostreet")).click();
-    //     element(by.partialButtonText("Load Scene")).click();
-    //     browser.sleep(2000);
-    //
-    //     browser.actions()
-    //         .mouseMove({x: 300, y: 300})
-    //         .mouseDown()
-    //         .mouseMove({x: 50, y: 50})
-    //         .mouseUp()
-    //         .perform();
-    //
-    //     browser.sleep(500);
-    //
-    //     element(by.cssContainingText("option", "Coverage")).click();
-    //
-    //     browser.sleep(2000);
-    // });
+    it("shoud show the District option", () => {
+        element(by.id("select-district")).click();
+        element(by.partialButtonText("Load Scene")).click();
+        browser.sleep(2000);
+
+        browser.actions()
+            .mouseMove({x: 300, y: 300})
+            .mouseDown()
+            .mouseUp()
+            .perform();
+
+        browser.sleep(500);
+
+        element(by.cssContainingText("option", "Number of authors")).click();
+
+        browser.sleep(10000);
+    });
+
+    it("shoud show the District option with the leak period profile", () => {
+        element(by.id("select-district")).click();
+        element(by.cssContainingText("option", "Leak period")).click();
+
+        element(by.partialButtonText("Load Scene")).click();
+        browser.sleep(2000);
+
+        browser.actions()
+            .mouseMove({x: 300, y: 300})
+            .mouseDown()
+            .mouseUp()
+            .perform();
+
+        browser.sleep(500);
+
+        element(by.cssContainingText("option", "Coverage")).click();
+
+        browser.sleep(2000);
+
+        // element(by.partialButtonText("Share")).getLocation().then((location) => {
+        //     browser.actions()
+        //         .mouseMove(location)
+        //         .perform();
+        //     element(by.partialButtonText("Open in new tab")).click();
+        //     browser.sleep(5000);
+        // });
+    });
+
+    it("shoud show the evostreet option", () => {
+        element(by.id("select-evostreet")).click();
+        element(by.partialButtonText("Load Scene")).click();
+        browser.sleep(2000);
+
+        browser.actions()
+            .mouseMove({x: 300, y: 300})
+            .mouseDown()
+            .mouseMove({x: 50, y: 50})
+            .mouseUp()
+            .perform();
+
+        browser.sleep(500);
+
+        element(by.cssContainingText("option", "Coverage")).click();
+
+        browser.sleep(2000);
+    });
 });
