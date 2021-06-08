780e54c54b8e1ba5e37864eaee6f62bb74947670
stefan <stefan@rinderle.info>
2021-06-08 21:19:37 +0200

    try to fix jenkins build

--- sonar-softvis3d-plugin/src/it/java/de/rinderle/softvis3d/SonarQubeContainer.java
+++ sonar-softvis3d-plugin/src/it/java/de/rinderle/softvis3d/SonarQubeContainer.java
@@ -48,7 +48,6 @@ public class SonarQubeContainer extends GenericContainer<SonarQubeContainer> {
 
         this.withNetwork(network);
         this.withNetworkAliases("sq_server");
-//        this.withImagePullPolicy(PullPolicy.alwaysPull());
 
         this.waitStrategy = new HttpWaitStrategy()
                 .forPath("/api/system/status")
